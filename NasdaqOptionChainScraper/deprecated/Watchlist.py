15891890177d70afc72a5a03fe2752766bcfac36
trentsteel777 <trentsteel777@gmail.com>
2017-07-08 22:42:54 +0100

    Remaining changes - Deleted autotask app and also NasdaqOptionChainScraper folder because the code has been moved into analysisportal

--- NasdaqOptionChainScraper/deprecated/Watchlist.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.utils import timezone
-
-class Watchlist(models.Model):
-    name = models.CharField(max_length=45)
-
-    def __str__(self):
-        return self.name
--- autotask/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AutotaskConfig(AppConfig):
-    name = 'autotask'
