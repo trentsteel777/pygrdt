939e843a76a317de246c6e6a79aff6106ceb1307
trentsteel777 <trentsteel777@gmail.com>
2017-07-08 22:47:11 +0100

    Removed bin folder

--- bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/trent/Workspace/django_virtual_environments/pygrdtvenv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
