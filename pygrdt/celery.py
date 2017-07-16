fa2b93551b64e635fe1fd34ac3535d6064614599
trentsteel777 <trentsteel777@gmail.com>
2017-07-16 23:13:33 +0100

    celery.py

---
+++ pygrdt/celery.py
@@ -0,0 +1,30 @@
+from __future__ import absolute_import, unicode_literals
+from celery import Celery
+from celery.schedules import crontab
+import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pygrdt.settings')
+
+app = Celery('pygrdt')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+
+app.conf.timezone = 'UTC'
+
+app.conf.beat_schedule = {
+    'run-nasdaq-scraper': {
+        'task': 'analysisportal.tasks.scrapeNasdaqWebsite',
+        # Execute every hour between 3pm and 9pm on weekdays only
+        'schedule': crontab(),
+        #'schedule': crontab(hour='*/1,15-21', day_of_week='mon,tue,wed,thu,fri'),
+    },
+    'add-every-monday-morning': {
+        'task': 'analysisportal.tasks.add',
+        'schedule': crontab(),
+        'args': (16, 16),
+    },
+}
+
+
+app.autodiscover_tasks()
